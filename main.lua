93e98a8ef651d2b7621da56662c6f919adba2c68
Cathetus <Cathetus@users.noreply.github.com>
2015-08-07 20:58:49 +0000

    Renamed to assets

--- main.lua
+++ main.lua
@@ -11,15 +11,15 @@ end
 function love.load()--loading the images and setting up the board
 	love.window.setMode(360,360)
 
-	ls = love.graphics.newImage("data/light_square.png")
-	ds = love.graphics.newImage("data/dark_square.png")
+	ls = love.graphics.newImage("assets/light_square.png")
+	ds = love.graphics.newImage("assets/dark_square.png")
 	
-	p = love.graphics.newImage("data/pawn.png")
-	n = love.graphics.newImage("data/knight.png")
-	b = love.graphics.newImage("data/bishop.png")
-	r = love.graphics.newImage("data/rook.png")
-	q = love.graphics.newImage("data/queen.png")
-	k = love.graphics.newImage("data/king.png")
+	p = love.graphics.newImage("assets/pawn.png")
+	n = love.graphics.newImage("assets/knight.png")
+	b = love.graphics.newImage("assets/bishop.png")
+	r = love.graphics.newImage("assets/rook.png")
+	q = love.graphics.newImage("assets/queen.png")
+	k = love.graphics.newImage("assets/king.png")
 	
 	
 	tiles = {ls,ds,ls,ds,ls,ds,ls,ds,ls,ds,ls,ds,ls,ds,ls,ds}
