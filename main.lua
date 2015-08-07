45a65e10c5496c74672ab2cb82cc0586f0f1deae
Cathetus <Cathetus@users.noreply.github.com>
2015-08-07 22:19:14 +0000

    Update code

--- main.lua
+++ main.lua
@@ -5,13 +5,13 @@ function love.load()
 	game.shuffle(board)
 end
 
+function love.draw()
+	game.draw()
+end
+
 --Space to shuffle the board
 function love.keypressed(key, isrepeat)
 	if key == " " then
 		game.shuffle(board)
 	end
-end
-
-function love.draw()
-	game.draw()
 end
